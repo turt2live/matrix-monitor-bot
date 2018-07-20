f055950b7c3b839f82b027f2181cad801c5e5f3f
Travis Ralston <travpc@gmail.com>
2018-07-20 17:46:47 -0600

    copy/paste fail

--- src/github.com/turt2live/matrix-monitor-bot/metrics/ping_handler.go
+++ src/github.com/turt2live/matrix-monitor-bot/metrics/ping_handler.go
@@ -9,6 +9,6 @@ type PingHandler struct{}
 
 func (PingHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
-	io.WriteString(w, "react-sdk-config-server is alive")
+	io.WriteString(w, "matrix-monitor-bot is alive")
 	return
 }
